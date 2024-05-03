d2afb7e07792bf2397c15f5c8c0374f957a72e50
emin <eminozgur04@gmail.com>
2024-05-03 21:11:53 +0300

    userName ve şifre ile girişi hallettim

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,30 +5,38 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class HelloController {
+
+    private final String userName = "ajenkeÇakan";
+    private final int pass = 31;
     @FXML
-    private Label welcomeText;
+    private Button button;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private PasswordField passField;
 
-
+    @FXML
+    private TextField userField;
     public void tryLogin(ActionEvent event) throws IOException {
+        if(userField.getText().equals(userName) && Integer.parseInt(passField.getText()) == pass){
             Parent home_page = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
             Scene hp_scene = new Scene(home_page);
             Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             app_stage.setScene(hp_scene);
             app_stage.show();
+        }
+
+
 
             }
     }
