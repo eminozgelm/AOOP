d5fb3345f55688b3c7c95a81d8ecc7d35391435f
Ajenk G. Kamalı <113362759+Aqenq@users.noreply.github.com>
2024-05-03 19:52:10 +0300

    başladık evelallah

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.stage.Stage;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -11,4 +16,14 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-}
+
+
+    public int tryLogin(String username,String pasw) {
+        if (username == "ajenk" && pasw == "123"){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    }
