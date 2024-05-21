80a9a93b47cd576e374dcce046e1dfd8607c24ce
ardaglnz <ardagulnaz@outlook.com>
2024-05-21 17:40:59 +0300

    group classi

---
+++ src/main/java/com/example/demo/Group.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class Group {
+    private String groupName;
+    private int[] usersArray;
+
+    private int[] groupAdmins;
+    public Group(String groupName, int[] usersArray, int[] groupAdmins) {
+        this.groupName = groupName;
+        this.usersArray = usersArray;
+        this.groupAdmins = groupAdmins;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public int[] getUsersArray() {
+        return usersArray;
+    }
+
+    public void setUsersArray(int[] usersArray) {
+        this.usersArray = usersArray;
+    }
+
+    public int[] getGroupAdmins() {
+        return groupAdmins;
+    }
+
+    public void setGroupAdmins(int[] groupAdmins) {
+        this.groupAdmins = groupAdmins;
+    }
+
+
+}
